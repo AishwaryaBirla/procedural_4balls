3f5368da199a703bc2ac6ba9987070301283421a
AishwaryaBirla <53036180+AishwaryaBirla@users.noreply.github.com>
2022-02-21 15:28:59 +0530

    procedural

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -2,7 +2,9 @@ import processing.core.PApplet;
 
 public class TryProcessing extends PApplet {
 
-    int a=0,b=0,c=0,d=0;
+    public static final int WIDTH = 649;
+    public static final int HEIGHT = 480;
+    int a=0;
     public static void main(String[] args) {
         PApplet.main("TryProcessing", args);
 
@@ -11,7 +13,7 @@ public class TryProcessing extends PApplet {
     @Override
     public void settings() {
         super.settings();
-        size(640,480);
+        size(WIDTH, HEIGHT);
 
     }
 
@@ -25,18 +27,15 @@ public class TryProcessing extends PApplet {
     @Override
     public void draw() {
 
-        ellipse(a,96,15,15);
+        int height = HEIGHT / 5;
+        getEllipse(a, height);
+        getEllipse(a*2, height*2);
+        getEllipse(a*3, height*3);
+        getEllipse(a*4, height*4);
         a++;
-        ellipse(b,192,15,15);
-        b+=2;
-        ellipse(c,288,15,15);
-        c+=3;
-        ellipse(d,384,15,15);
-        d+=4;
-        if (a==width) a=0;
-        if (b==width) b=0;
-        if (c==width) c=0;
-        if (d==width) d=0;
+    }
 
+    private void getEllipse(int height, int width) {
+        ellipse(height, width,15,15);
     }
 }
